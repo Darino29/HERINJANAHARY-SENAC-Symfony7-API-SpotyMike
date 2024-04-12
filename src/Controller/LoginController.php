6cdd51b422566fd3c0076a86f5e324684b0f33f3
email <senac@et.intechinfo.fr>
2024-04-12 16:45:14 +0200

    Gestion erreur Register (suppression lignes superflu)

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -128,47 +128,7 @@ if (preg_match('/^\d{2}\/\d{2}\/\d{4}$/', $dateOfBirthString)) {
 
         
         
-        // Valider l'utilisateur avec le Validator
-        $errors = $validator->validate($user);
-
-        // Si des erreurs de validation sont présentes
-        if (count($errors) > 0) {
-            $errorMessages = [];
-            foreach ($errors as $error) {
-                $errorMessages[$error->getPropertyPath()] = $error->getMessage();
-            }
-            return $this->json([
-                'error' => true,
-                'message' => 'Format des données invalides',
-                'details' => $errorMessages,
-            ], 400);
-        }
-
-        // Vérifier si l'utilisateur a moins de 12 ans
-        $age = date_diff(new DateTime(), $dateOfBirth)->y;
-        if ($age < 12) {
-            return $this->json([
-                'error' => true,
-                'message' => 'Âge minimum non respecté (moins de 12 ans)',
-            ], 400);
-        }
-
         
-        if (empty($data['email'])) {
-            return $this->json([
-                'error' => true,
-                'message' => "Le champ 'email' ne peut pas être vide",
-            ], 400);
-        }
-
-        // Vérifier si l'email est déjà utilisé
-        $existingUser = $this->repository->findOneBy(['email' => $data['email']]);
-        if ($existingUser) {
-            return $this->json([
-                'error' => true,
-                'message' => 'Email déjà utilisé',
-            ], 400);
-        }
         $password = $data['password'];
           if (preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,}$/', $password)) {
             $password = $data['password'] ?? ''; 
@@ -199,17 +159,17 @@ if (preg_match('/^\d{2}\/\d{2}\/\d{4}$/', $dateOfBirthString)) {
     #[Route('/login', name: 'app_login_post', methods: ['POST'])]
     public function login(Request $request, JWTTokenManagerInterface $JWTManager): JsonResponse
     {
-        // Récupérer les données JSON du corps de la requête
+       
         $data = json_decode($request->getContent(), true);
     
-        // Récupérer l'email et le mot de passe depuis les données
+
         $email = $data['email'] ?? '';
         $password = $data['password'] ?? '';
     
-        // Rechercher l'utilisateur par son email dans la base de données
+    
         $user = $this->repository->findOneBy(["email" => $email]);
     
-        // Vérifier si un utilisateur correspondant a été trouvé
+    
         if (!$user) {
             return $this->json(['message' => 'Identifiants invalides'], 401);
         }
